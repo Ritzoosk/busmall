651536df53c06f9b198813f539831910947109f3
Ritzoosk <knecht.ted@gmail.com>
2021-01-29 18:39:00 -0800

    added readme comments and removed spaces and console prints

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # busmall
 SkyMall ground version
+
+> this app randomly selects 3 images to present to the user. Users clicks the selected "desired image, that click is tallied and stored for use in the generation of a data chart presenting the clicked images along with the amount of times they were seen.
+
+
+# resources:
+- https://www.chartjs.org/
--- app.js
+++ app.js
@@ -17,13 +17,11 @@ function StoredProduct(name) {
   this.timesShown = 0;
   this.timesClicked = 0;
   this.image = `assets/${name}`;
-
   StoredProduct.allImages.push(this);
   StoredProduct.imageMap[this.name] = this;
 }
 StoredProduct.allImages = [];
 StoredProduct.imageMap = {};
-
 
 for (var i = 0; i < products.length; i++){
   new StoredProduct(products[i]);
@@ -32,45 +30,27 @@ for (var i = 0; i < products.length; i++){
 //Returns array with images//
 function threeRndImg(){
   console.log('last set of pics',lastImg );
-
   tempArray[0] = getRnd();
   var leftImg = StoredProduct.allImages[tempArray[0]]
-
-  //console.log('1st # is', tempArray[0]);
- 
   tempArray[1] = getRnd();
   var centerImg = StoredProduct.allImages[tempArray[1]]
-
-  //console.log('2nd # is',tempArray[1] );
-
   tempArray[2]  = getRnd();
   var rightImg = StoredProduct.allImages[tempArray[2]]
-
   console.log('pics selected',tempArray );
-  //console.log('last set of pics',lastImg );
-
   lastImg = tempArray;
-
-  //console.log('lastImg',lastImg );
-
   return[leftImg, centerImg, rightImg];
 }
 
 //Get and Check random number viability//
 function getRnd(){
   var rndNum = Math.floor(Math.random() * StoredProduct.allImages.length);
-
-   //console.log('rndNum initially is',rndNum);
   while(lastImg.includes(rndNum)||tempArray.includes(rndNum)){
     console.log('duplicate #', rndNum);
     rndNum = getRnd();
-    //console.log(typeof imgOne);
      console.log('needed a new #', rndNum);
   }
   return(rndNum);
 }
- //console.log(StoredProduct.allImages);
-
 
 //DOM Elements//
 var imgContainter = document.getElementById('img-container');
@@ -81,30 +61,23 @@ function renderTrio(leftImg, centerImg, rightImg){
   var imgCalled1 = document.getElementById('img-called1');
   var imgCalled2 = document.getElementById('img-called2');
   var imgCalled3 = document.getElementById('img-called3');
-  
   imgCalled1.src = leftImg.image;
   leftImg.timesShown++;
-
   imgCalled2.src = centerImg.image;
   centerImg.timesShown++;
-
   imgCalled3.src = rightImg.image;
   rightImg.timesShown++;
-
 }
 
 //Initialize//
 var randomImgs = threeRndImg();
 renderTrio(randomImgs[0],randomImgs[1],randomImgs[2]);
-
-
 var buttonElement = document.getElementById('submitButton')
 
 
 //Detect Click//
 imgContainter.addEventListener('click', function (event) {
   console.log(event.target); 
-
   for (var i = 0; i < StoredProduct.allImages.length; i++) {
     if (event.target.src.includes(StoredProduct.allImages[i].image)) {
       StoredProduct.allImages[i].timesClicked++;
@@ -118,10 +91,7 @@ imgContainter.addEventListener('click', function (event) {
   }else{
     console.log('DUN!');
     imgContainter.style.display='none';
-
     buttonElement.style.display= 'inline-block';
-    //printResults();
-    //buttonElement is Global//
     return buttonElement;
   }
 });
@@ -147,31 +117,25 @@ console.log('initialized arry' , lastShows);
 //Submit Results Listener??
 buttonElement.addEventListener('click', function (event) {
   event.preventDefault();
- 
   var ctx = document.getElementById('myChart').getContext('2d');
   var votesByProduct = [];
   var timesShownChart = [];
   var cleanNames = [];
-
    var lastVotes = localStorage.getItem('votes')||'[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]';
   lastVotes = JSON.parse(lastVotes);
   console.log('lastVotes', lastVotes);
-
   var lastShows = localStorage.getItem('shown')||'[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]';
   lastShows = JSON.parse(lastShows);
   console.log('lastShows', lastShows);
-
-
   for(var  i=0; i < StoredProduct.allImages.length; i++){
 
     votesByProduct.push((StoredProduct.allImages[i].timesClicked) + lastVotes[i]);
     timesShownChart.push((StoredProduct.allImages[i].timesShown) + lastShows[i]);
     cleanNames.push(StoredProduct.allImages[i].nameImg);
   }
+
   console.log('votes', votesByProduct);
   console.log('shown', timesShownChart);
-
-
   var votesByString = JSON.stringify(votesByProduct);
   localStorage.setItem('votes', votesByString);
   var shownString = JSON.stringify(timesShownChart);
@@ -294,7 +258,6 @@ buttonElement.addEventListener('click', function (event) {
     }
     }
   });
-  
 });
 
 
